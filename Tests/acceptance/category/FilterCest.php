18e6c21a9f6db3d97e4d4fa5db808844217bf518
Roland Golla <rolandgolla@gmail.com>
2019-03-14 14:22:58 +0100

    Add reset filter test

--- Tests/acceptance/category/FilterCest.php
+++ Tests/acceptance/category/FilterCest.php
@@ -16,13 +16,35 @@ class FilterCest
     public function filterReduceResults(AcceptanceTester $I, Category $page)
     {
         $actualResult = (int)$I->grabTextFrom($page::$counter);
+        $this->clickFirstFilterElements($I, $page);
+        $newResult = (int)$I->grabTextFrom($page::$counter);
+        $I->assertLessThan($actualResult,$newResult);
+    }
+
+    public function filterReset(AcceptanceTester $I, Category $page)
+    {
+        $actualResult = (int)$I->grabTextFrom($page::$counter);
+        $this->clickFirstFilterElements($I, $page);
+        $I->click($page::$restButton);
+        $I->wait(1);
+        $I->assertSame($actualResult, (int)$I->grabTextFrom($page::$counter));
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     * @param Category $page
+     * @param $filterFirst
+     * @param $filterFirstCheckbox
+     * @param $loadingSpinner
+     * @throws \Exception
+     */
+    protected function clickFirstFilterElements(AcceptanceTester $I, Category $page)
+    {
         $I->scrollTo($page::$filterFirst);
         $I->click($page::$filterFirst);
         $I->wait(1);
         $I->click($page::$filterFirstCheckbox);
         $I->waitForElementVisible($page::$loadingSpinner);
         $I->waitForElementNotVisible($page::$loadingSpinner);
-        $newResult = (int)$I->grabTextFrom($page::$counter);
-        $I->assertLessThan($actualResult,$newResult);
     }
 }
