a25814441c7ee2c64bbff045aac84b0e9c323e04
Roland Golla <rolandgolla@gmail.com>
2019-03-14 15:50:45 +0100

    Test api post without valid keys

--- Tests/api/MesssageCest.php
+++ Tests/api/MesssageCest.php
@@ -31,6 +31,38 @@ class MesssageCest
             'message' => 'email|17624747727|Testify',
             'name' => 'Roland Golla ' . $time
         ];
+
         $I->postHandler('/api/messages', $data, 400);
+    }
+
+    public function emptyValueDontGoToDatabase(ApiTester $I) {
+        $time = microtime();
+        $data = [
+            'email' => 'rolandgolla@gmail.com',
+            'message' => 'email|17624747727|Testify',
+            'name' => 'Roland Golla ' . $time
+        ];
+
+        foreach ($data as $key => $value) {
+            $tempData = $data;
+            $tempData[$key] = '';
+
+            $I->postHandler('/api/messages', $tempData, 400);
+        }
+    }
+
+    public function emptyKeyDontGoToDatabase(ApiTester $I)
+    {
+        $time = microtime();
+        $data = [
+            'email' => 'rolandgolla@gmail.com',
+            'message' => 'email|17624747727|Testify',
+            'name' => 'Roland Golla ' . $time
+        ];
+        foreach ($data as $key => $value) {
+            $temporaryData  =  $data;
+            unset($temporaryData[$key]);
+            $I->postHandler('/api/messages', $temporaryData, 401);
+        }
     }
 }
